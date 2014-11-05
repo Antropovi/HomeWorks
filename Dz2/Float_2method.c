89444aeaa5f23bb8c520917c30c171472f8ca878
Antropovi <antropovi@yahoo.com>
2014-11-05 14:22:14 +0300

    Update Float_2method.c

--- Dz2/Float_2method.c
+++ Dz2/Float_2method.c
@@ -19,8 +19,20 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	MyStruct val;
 	scanf("%f", &val.val.fval);
-	printf("sign = %lu,  exp = %lu,   mantisa = %lu", (val.val.ival>>31) & 0x01,
-  (val.val.ival >>23) & 0xff, val.val.ival  & 0x007fffff);
+	unsigned exp = (val.val.ival >>23) & 0xff;
+	unsigned sign = (val.val.ival>>31) & 0x01;
+	unsigned mantissa=(val.val.ival  & 0x007fffff);
+	if (!exp && mantissa) {
+		if (sign) printf ("infinity"); else printf ("-infinity");
+	}
+	else 
+		if (exp && !mantissa) printf("NaN");
+		else 
+			if (!exp && !mantissa) printf("fval=0");
+			else
+			{
+				printf("fval = (-1)^%d*2^%d*1.%lu ", sign, exp-127, mantissa);
+			}
 	getch();
 	return 0;
 }
