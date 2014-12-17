e1c041014d5f7d9789d171cd47abdd344315b97e
Antropovi <antropovi@yahoo.com>
2014-12-17 14:43:31 +0300

    Updated

--- Dz4/Searching.c
+++ Dz4/Searching.c
@@ -4,33 +4,33 @@
 #include "stdafx.h"
 #include <string.h>
 
-int finding (int i, char FirstString[100], char SecondString[100])
+int copmpare(int i, char firststring[100], char secondstring[100])
 {
     int j = 0;
 
-    for (j = 0; SecondString[j] != 0 && FirstString[j+i] != 0; j++)
+    for (j = 0; secondstring[j] != 0 && firststring[j+i] != 0; j++)
     {
-        if (FirstString[j+i] != SecondString[j]) return 0;
+        if (firststring[j+i] != secondstring[j]) return 0;
     }
 
-    return SecondString[j] == 0 ? 1 : 0;
+    return secondstring[j] == 0 ? 1 : 0;
 
 }
 
 
 int main()
 {
-	char FirstString[100] = "";
-	char SecondString[100] = "";
+	char firststring[100] = "";
+	char secondstring[100] = "";
     
-	scanf("%s", FirstString);
-	scanf("%s", SecondString);
+	scanf("%s", firststring);
+	scanf("%s", secondstring);
 	int i = 0, count = 0;
-	for (i = 0; FirstString[i] != 0; i++)
+	for (i = 0; firststring[i] != 0; i++)
 	{
-		if (FirstString[i]==SecondString[0]) 
+		if (firststring[i]==secondstring[0]) 
 		{
-			if ( finding(i,FirstString,SecondString) )
+			if (copmpare(i,firststring,secondstring))
 			{
 				++count;
 			}
