5e859bcbe227799837e770546122130f9b1f7b07
Antropovi <antropovi@yahoo.com>
2014-12-17 14:46:36 +0300

    Update Float_2method.c

--- Dz2/Float_2method.c
+++ Dz2/Float_2method.c
@@ -2,7 +2,6 @@
 //
 
 #include "stdafx.h"
-#include <conio.h>
 
 struct MyStruct
 {
@@ -14,29 +13,40 @@ struct MyStruct
 
 };
 
-int _tmain(int argc, _TCHAR* argv[])
+int main()
 {
-
 	MyStruct val;
-	float a,b;
-	scanf("%f",&a);
-	scanf("%f",&b);
-	val.val.fval=a/b;
-	unsigned exp = (val.val.ival >>23) & 0xff;
-	unsigned sign = (val.val.ival>>31) & 0x01;
-	unsigned mantissa=(val.val.ival  & 0x007fffff);
-	if (exp==255 && !mantissa) {
+	float valin = 0, valout = 0;
+	scanf("%f",&valin);
+	scanf("%f",&valout);
+	
+	val.val.fval = valin / valout;
+	unsigned exp = (val.val.ival >> 23) & 0xff;
+	unsigned sign = (val.val.ival > >31) & 0x01;
+	unsigned mantissa = (val.val.ival & 0x007fffff);
+	
+	if (exp==255 && !mantissa) 
+	{
 		if (!sign) printf ("infinity"); else printf ("-infinity");
 	}
 	else 
-		if (exp==255 && mantissa) printf("NaN");
-		else 
-			if (!exp && !mantissa) printf("fval=0");
+	{
+		if (exp==255 && mantissa) 
+		{
+			printf("NaN");
+		}
+		else
+		{
+			if (!exp && !mantissa)
+			{
+				printf("fval=0");
+			}
 			else
 			{
 				printf("fval = (-1)^%d*2^%d*1.%lu ", sign, exp-127, mantissa);
 			}
-	getch();
+		}
+	}	
 	return 0;
 }
 
