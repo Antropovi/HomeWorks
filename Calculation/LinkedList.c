a4e5c99ed51cb129cd4999b50dcd1beb4dceff5a
Antropovi <antropovi@yahoo.com>
2014-12-24 07:08:17 +0300

    Update LinkedList.c

--- Calculation/LinkedList.c
+++ Calculation/LinkedList.c
@@ -5,36 +5,40 @@
 
 
 void linkedList_Revert(struct node ** head){
- struct node * prev = NULL;
- struct node * next = NULL;
- struct node * curr = *head;
+ 	struct node * prev = NULL;
+ 	struct node * next = NULL;
+ 	struct node * curr = *head;
 
- while (curr) {
-  next = curr->next;
-  curr->next = prev;
-  prev = curr;
-  curr = next;
- }
+	 while (curr) 
+	 {
+  	 	next = curr->next;
+ 		curr->next = prev;
+ 		prev = curr;
+ 		curr = next;
+	 }
 
- *head = prev;
+ 	*head = prev;
 }
 
 
-void linkedList_Add(int val, struct node ** head){
- struct node * new_node = (struct node*)malloc(sizeof(struct node));
- new_node->next = *head;
- *head = new_node;
- new_node->val = val;
+void linkedList_Add(int val, struct node ** head)
+{
+	struct node * new_node = (struct node*)malloc(sizeof(struct node));
+	new_node->next = *head;
+	*head = new_node;
+	new_node->val=val;
 }
 
-void linkedList_Insert(struct node ** head){
- struct node * new_node = (struct node*)malloc(sizeof(struct node));
- new_node->next = NULL;
- *head = new_node;
- new_node->val = 0;
+void linkedList_Insert(struct node ** head)
+{
+	struct node * new_node = (struct node*)malloc(sizeof(struct node));
+	new_node->next = NULL;
+	*head = new_node;
+	new_node->val = 0;
 }
 
-void linkedList_Print(struct node ** head){
+void linkedList_Print(struct node ** head)
+{
 
 	struct node * curr;
 	struct node * temp;
@@ -47,21 +51,21 @@ void linkedList_Print(struct node ** head){
 		free(temp);
 	}
 
-	//linkedList_Revert(&(head));
-
 	printf("%d", (*head)->val);
 	curr = (*head)->next;
-	while (curr) {
+	while (curr) 
+	{
 		printf("%03d" , curr->val);
-		curr=curr->next;
+		curr = curr->next;
 	}
-	printf(" ");
 }
 
-void linkedList_Free(struct node *head){
+void linkedList_Free(struct node *head)
+{
 	struct node* next;
 	struct node* n = head;
-	while(n!=NULL){
+	while(n != NULL)
+	{
 		next = n->next;
 		free(n);
 		n = next;
