0226e8ef91fe45111903960f0a5b0df9b5522cf4
Antropovi <antropovi@yahoo.com>
2014-12-24 06:51:10 +0300

    Update LongMath.h

--- Dz6/LongMath.h
+++ Dz6/LongMath.h
@@ -1,8 +1,5 @@
 
-
-
-
-struct number *LongMath_plus(struct number *num1, struct number *num2);
-struct number *LongMath_minus(struct number *num1, struct number *num2);
-struct number *LongMath_multi(struct number *num1, struct number *num2);
-struct number *LongMath_div(struct number *num1, struct number *num2);
+struct number *longMath_Plus(struct number *num1, struct number *num2);
+struct number *longMath_Minus(struct number *num1, struct number *num2);
+struct number *longMath_Multi(struct number *num1, struct number *num2);
+struct number *longMath_Div(struct number *num1, struct number *num2);
