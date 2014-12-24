ee84002061e5c3b70e37ecb5ef3648429bba56a0
Antropovi <antropovi@yahoo.com>
2014-12-24 07:10:22 +0300

    Update LongAlg.c

--- Calculation/LongAlg.c
+++ Calculation/LongAlg.c
@@ -1,25 +1,28 @@
-// LongAlg.c Стековый калькулятор Антропов ИМ
+// LongAlg.c : РђРЅС‚СЂРѕРїРѕРІ РРіРѕСЂСЊ РњРёС…Р°Р№Р»РѕРІРё РЎС‚РµРєРѕРІС‹Р№ РєР°Р»СЊРєСѓР»СЏС‚РѕСЂ
 //
 
-#include <conio.h>
 #include <stdlib.h>
 #include <string.h> 
 #include "stdafx.h"
 
-int read_lexem(struct node **lexem){
-	char c[2];
+int read_lexem(struct node **lexem)
+{
+	char temp[2];
 
 	*lexem = NULL;
 	
-	do{
-		fgets(c, 2, stdin);
-	}while(c[0]==' ' || c[0]=='\n' || c[0]=='\r');
+	do
+	{
+		fgets(temp, 2, stdin);
+	}while(temp[0] == ' ' || temp[0] == '\n' || temp[0] == '\r');
 
-	while(c[0]!=' ' && c[0]!='\n' && c[0]!='\r'){
-		linkedList_Add(c[0], lexem);
-		fgets(c, 2, stdin);
+	while(temp[0] != ' ' && temp[0] != '\n' && temp[0] != '\r')
+	{
+		linkedList_Add(temp[0], lexem);
+		fgets(temp, 2, stdin);
 	}
-	return c[0]!='\n' && c[0]!='\r';
+
+	return temp[0] != '\n' && temp[0] != '\r';
 }
 
 int main()
@@ -27,30 +30,27 @@ int main()
 	struct node *lexem;
 	struct stack *stack = NULL;
 	int flag_read_next;
-	int flag_do_process = 1;//true
+	int status = 0;
 
 	flag_read_next = read_lexem(&lexem);
-	while(lexem!=NULL){
-		stack = stack_Add(stack, lexem);
-		
-		// если расчет еще не сломался
-		// то прасчитываем дальше
-		if(flag_do_process)
-			flag_do_process = stack_Process(&stack);
+	while(lexem != NULL && status == 0)
+	{
+		if ('0' <= lexem->val && lexem->val <= '9')
+			stack = stack_Add(stack, lexem);
+		else
+			status = stack_Process(&stack, lexem->val) == 0 ? 1 : 0;
 
-		// если не прочли все до конца
-		// то читаем дальше
 		if(flag_read_next)
 			flag_read_next = read_lexem(&lexem);
 		else
-			lexem=NULL;
+			lexem = NULL;
 	}
 
-	stack_Print(stack);
+	if(status == 0)
+		stack_Print(stack);
 
 	stack_Free(stack);
 
-	getch();
-	return 0;
+	return status;
 }
 
