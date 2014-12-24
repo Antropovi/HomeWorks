ec461fbb9965e32f181d02c789648c24f93f6b32
Antropovi <antropovi@yahoo.com>
2014-12-24 06:52:18 +0300

    Update Numbers.c

--- Dz6/Numbers.c
+++ Dz6/Numbers.c
@@ -1,10 +1,10 @@
-#include <conio.h>
 #include <stdlib.h>
 #include <string.h> 
 #include "stdafx.h"
 
 
-struct number* numbers_Init(){
+struct number* numbers_Init()
+{
 	struct number* new_number = (struct number*)malloc(sizeof(struct number));
 	new_number->sign = 1;
 	new_number->head = NULL;
@@ -12,32 +12,33 @@ struct number* numbers_Init(){
 }
 
 
-void numbers_Read(struct number *num, char *string){
-	int val;
-	int i=0;
-	if (string[0]=='-') {
-		num->sign=-1;
-		string[0]='0';
-	}
-	else if (string[0]=='+')
+void numbers_Read(struct number *num)
+{
+	char temp[2];
+
+	do
 	{
-		string[0]='0';
-	}
-	for (i=strlen(string)-1; i>=0; i=i-3)
+		fgets(temp, 2, stdin);
+	} while(temp[0] == ' ' || temp[0] == '\n' || temp[0] == '\r');
+
+	while(temp[0] != ' ' && temp[0] != '\n' && temp[0] != '\r')
 	{
-		val = string[i]-'0' +10* (i-1>=0 ? (string[i-1]-'0') : 0)+100* (i-2>=0 ? (string[i-2]-'0') : 0);
-		linkedList_Add(val, &(num->head));
+		linkedList_Add(temp[0], &(num->head));
+		fgets(temp, 2, stdin);
 	}
+
 	linkedList_Revert(&(num->head));
 }
 
-void numbers_Free(struct number* num){
-	 struct node* next;
+void numbers_Free(struct number* num)
+{
+	struct node* next;
 	struct node* n = num->head;
-	while(n!=NULL){
-	next = n->next;
-	free(n);
-	n=next;
+	while(n != NULL)
+	{
+		next = n->next;
+		free(n);
+		n = next;
 	}
-  free(num);
+  	free(num);
 }
