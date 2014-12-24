fe0dfdf80a20cfd7c6c8d6e4be0f3cfd5560a331
Antropovi <antropovi@yahoo.com>
2014-12-24 06:52:47 +0300

    Update Numbers.h

--- Dz6/Numbers.h
+++ Dz6/Numbers.h
@@ -6,5 +6,5 @@ struct number
 };
 
 struct number* numbers_Init();
-void numbers_Read(struct number *num, char *string);
+void numbers_Read(struct number *num);
 void numbers_Free(struct number* num);
